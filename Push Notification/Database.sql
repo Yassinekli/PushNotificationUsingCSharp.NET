f2898867a956f6c9753a6fb29d67b0ce8f5ee562
Yassine KLILICH <klilichyassine8@gmail.com>
2018-08-24 23:42:56 +0100

    Update Database.sql

--- Push Notification/Database.sql
+++ Push Notification/Database.sql
@@ -3,7 +3,7 @@ create database [Test Push Notification DB]
 USE [Test Push Notification DB]
 
 CREATE TABLE [dbo].[SubscribersSet](
-	[Subscriber_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Subscriber_ID] [int] IDENTITY(1,1) PRIMARY KEY,
 	[endPoint] [nvarchar](max) NOT NULL,
 	[auth] [nvarchar](max) NOT NULL,
 	[p256dh] [nvarchar](max) NOT NULL
